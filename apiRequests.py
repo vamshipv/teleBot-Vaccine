c438b72c2adab8fbb673aadaa5029078d1150cc8
prasad145 <acharyaprasad145@gmail.com>
2021-05-28 16:33:26 +0530

    api response fixed

--- apiRequests.py
+++ apiRequests.py
@@ -12,14 +12,14 @@ def writeJson(msg):
     with open("response.json", 'w') as f:
         json.dump(msg, f, indent=4)
 
-def getSlots(pinCode,date):
-    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pinCode, date)
+def getSlots(pinCode, date):
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(pinCode, date)
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     records = []
-    response = requests.get(URL,headers = header)
+    response = requests.get(URL, headers = header)
     if response.ok:
-        data_json = json.loads(response.text)
-        # print(data_json)
+        data_json = response.json()
+        print(data_json)
         for i in data_json['sessions']:
             hospitalName = i['name']
             available = i['available_capacity']
@@ -27,8 +27,8 @@ def getSlots(pinCode,date):
             brand = i['vaccine']
             records.append([hospitalName,available,ageLimit,brand])
     else:
-        print("Stay In Home Asshole")
+        print("Stay In Home, Wait for sometime!")
     return records 
 
-print(getSlots(577201,28-5-2021))
+# print(getSlots("577201", "28-5-2021"))
 
--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from flask.wrappers import Response
 import requests
-from werkzeug.utils import redirect
 import apiRequests
-
 
 app = Flask(__name__)
 
@@ -11,28 +9,26 @@ app = Flask(__name__)
 token = '1853149779:AAHrjUwlISb-VqGFcuK-oTu-pBjhPjhi-lI'
 
 # https://api.telegram.org/bot<token>/setWebhook?url=<ngrokURL>
+# https://api.telegram.org/bot1853149779:AAHrjUwlISb-VqGFcuK-oTu-pBjhPjhi-lI/setWebhook?url=https://f0f9359a56dd.ngrok.io
 
 # /getMe : A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
 check = 'https://api.telegram.org/bot{}/getMe'.format(token)
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def hello_world():
-    return render_template('home.html')
     # resp = requests.get(check) -> returns information about our bot
-    # if request.method == 'POST':
-    #     msg = request.get_json()
-    #     apiRequests.writeJson(msg)
-    #     print(msg)
-    #     return Response('ok', status = 200)
-
-@app.route('/home', methods=['GET','POST'])
-def home():
     if request.method == 'POST':
-        pinCode = request.form['pinCode']
-        date = request.form['date']
-    return render_template('home.html', pin = pinCode, da = date)
+        msg = request.get_json()
+        apiRequests.writeJson(msg)
+        print(msg)
+        return Response('ok', status=200) #informing telegram to we are getting response as we expected
+
+# @app.route('/home', methods=['GET','POST'])
+# def home():
+#     if request.method == 'POST':
+#         pinCode = request.form['pinCode']
+#         date = request.form['date']
+#     return render_template('home.html', pin = pinCode, da = date)
         
-
-
 if __name__ == "__main__":
     app.run(debug = True)
