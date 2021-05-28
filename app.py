55ca9e58af543c4ded7dbf4cb1f1164a072d399e
vamshipv <vamshi.pva123@gmail.com>
2021-05-28 12:47:16 +0530

    error rendering

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home',methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        pinCode = request.form.get('pinCode')
+        date = request.form.get('date')
+    return render_template('home.html',pinCode=pinCode,date=date)
 
 if __name__ == "__main__":
     app.run(debug = True)
