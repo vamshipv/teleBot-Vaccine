1403e2e2bcfffca1824fe4ebc40fa314f8b1517b
prasad145 <acharyaprasad145@gmail.com>
2021-05-28 20:45:01 +0530

    query for slots availability done!

--- apiRequests.py
+++ apiRequests.py
@@ -1,4 +1,5 @@
 import json
+from typing import Pattern
 from flask import Flask, request, render_template
 from flask.wrappers import Response
 import requests
@@ -6,6 +7,7 @@ from requests import status_codes
 from requests import sessions
 from werkzeug.utils import redirect
 import apiRequests
+import re
 import datetime
 
 def writeJson(msg):
@@ -19,7 +21,7 @@ def getSlots(pinCode, date):
     response = requests.get(URL, headers = header)
     if response.ok:
         data_json = response.json()
-        print(data_json)
+        # print(data_json)
         for i in data_json['sessions']:
             hospitalName = i['name']
             available = i['available_capacity']
@@ -27,8 +29,29 @@ def getSlots(pinCode, date):
             brand = i['vaccine']
             records.append([hospitalName,available,ageLimit,brand])
     else:
-        print("Stay In Home, Wait for sometime!")
-    return records 
+        return ""
+    return records
 
+def parseMsg(msg):
+    if 'message' in msg.keys():
+        chatID = msg['message']['chat']['id']
+        text = msg['message']['text']
+        # print(chatID, text)
+    else:
+        chatID = msg['edited_message']['chat']['id']
+        text = msg['edited_message']['text']
+    #regular expression for validating pincode
+    pattern = r'^/\d{6}$'
+    match = re.findall(pattern, text) # returns list of words in msg matching the pattern
+
+    pincode = ''
+    if match:
+        pincode = match[0][1:] #ignore '/'
+    return chatID, pincode
+
+def sendMsg(chatID, msg, token):
+    URL = 'https://api.telegram.org/bot{}/sendMessage'.format(token)
+    payload = {'chat_id' : chatID, 'text' : msg}
+    requests.post(URL, json=payload)
 # print(getSlots("577201", "28-5-2021"))
 
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from flask import Flask, request, render_template, redirect
+from flask.helpers import url_for
 from flask.wrappers import Response
 import requests
 import apiRequests
@@ -6,21 +8,52 @@ import apiRequests
 app = Flask(__name__)
 
 # Authentication token
-token = '1853149779:AAHrjUwlISb-VqGFcuK-oTu-pBjhPjhi-lI'
-
+token = '<token-ID>'
 # https://api.telegram.org/bot<token>/setWebhook?url=<ngrokURL>
-# https://api.telegram.org/bot1853149779:AAHrjUwlISb-VqGFcuK-oTu-pBjhPjhi-lI/setWebhook?url=https://f0f9359a56dd.ngrok.io
+# https://api.telegram.org/bot<token>/setWebhook?url=https://f0f9359a56dd.ngrok.io
 
 # /getMe : A simple method for testing your bot's auth token. Requires no parameters. Returns basic information about the bot in form of a User object.
-check = 'https://api.telegram.org/bot{}/getMe'.format(token)
+# check = 'https://api.telegram.org/bot{}/getMe'.format(token)
 
 @app.route('/', methods = ['POST'])
 def hello_world():
     # resp = requests.get(check) -> returns information about our bot
     if request.method == 'POST':
         msg = request.get_json()
-        apiRequests.writeJson(msg)
-        print(msg)
+        # apiRequests.writeJson(msg) -> to see the structure of reponse structure
+        # print(msg)
+        chatID, pinCode = apiRequests.parseMsg(msg)
+        if pinCode == '':
+            # print(chatID)
+            #sending error msg to the user
+            errorMsg = 'Incorrect command format.\nsend /<6-digit Pincode>'
+            apiRequests.sendMsg(chatID, errorMsg, token)
+        else:
+            for i in range(3):
+                curDate = datetime.today() + timedelta(i)
+                curDate = curDate.strftime('%d-%m-%Y')
+                slots = apiRequests.getSlots(pinCode, curDate)
+                if len(slots) == 0:
+                    apiRequests.sendMsg(chatID, "Date: {}\nNo Slots Available".format(curDate), token)
+                else:
+                    finalMsg = ""
+                    vis = 1
+                    endLine = "--------------------------------------\n"
+                    for slot in slots:
+                        if vis == 1:
+                            title = "Available Slot for Date: {}\n".format(curDate) + "\tCenter Name: {}\n".format(slot[0]) + "\tAvailability: {}\n".format(slot[1]) + "\tAge limit: {}\n".format(slot[2]) + "\tVaccine: {}\n".format(slot[3])
+                            finalMsg += title
+                            finalMsg += endLine
+                            vis += 1
+                        else:
+                            title = "\tCenter Name: {}\n".format(slot[0]) + "\tAvailability: {}\n".format(slot[1]) + "\tAge limit: {}\n".format(slot[2]) + "\tVaccine: {}\n".format(slot[3])
+                            finalMsg += title
+                            finalMsg += endLine
+                    apiRequests.sendMsg(chatID, finalMsg, token)
+            # curDate = datetime.today().strftime('%d-%m-%Y')
+            # print(curDate, pinCode)
+            # res = apiRequests.getSlots(pinCode, curDate)
+            # print(res)
         return Response('ok', status=200) #informing telegram to we are getting response as we expected
 
 # @app.route('/home', methods=['GET','POST'])
